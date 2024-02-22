2eb30aa7ff5b2b9c33b6c1c242662d4f237858a2
unknown <=>
2024-02-22 21:30:24 +0100

    changed backend i to https

--- src/http.ts
+++ src/http.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://18.222.82.204:4000/api", // The base URL for all requests made with this instance
+  baseURL: "https://18.222.82.204:4000/api", // The base URL for all requests made with this instance
   timeout: 60000 * 30, // Timeout for requests in milliseconds
   headers: {
     "Content-Type": "application/json",
